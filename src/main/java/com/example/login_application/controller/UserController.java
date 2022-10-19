fb84ad3c9103d0ed87d176764c6dffe3f199924f
trungtashi99 <trungtashi@gmai.com>
2022-10-19 08:57:34 +0700

    Pactice data binding login application

---
+++ src/main/java/com/example/login_application/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.login_application.controller;
+
+import com.example.login_application.model.Login;
+import com.example.login_application.model.User;
+import com.example.login_application.service.UserDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("home", "login", new Login());
+        return modelAndView;
+    }
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+        User user = UserDAO.checkLogin(login);
+        if(user == null){
+            ModelAndView modelAndView = new ModelAndView("error");
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
+    }
+}
